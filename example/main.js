d6448fff924bd5ef026f5a5519294567916fd84d
alexander-daniel <alex@lesinstruments.com>
2014-08-23 15:49:41 -0400

    updated example

--- example/main.js
+++ example/main.js
@@ -1,16 +1,14 @@
 'use strict';
 
-var loadingBars = require('../')({
-	canvasId: 'loading_animation',
-	maxBarHeight: 100,
-	Width:200,
-	animateSpeed:10
-});
+var LoadingBars = require('../');
 
-// on ready, start animatin' !
-$(function() {
-  loadingBars.startAnimation();
+var = loadingBars = new LoadingBars('loading_animation', {
+	width: 100,
+	height: 1000,
+	color: 'purple'
 });
+ 
+loadingBars.startAnimation();
 
 // stop animation after 5 seconds
 setTimeout(function () {
